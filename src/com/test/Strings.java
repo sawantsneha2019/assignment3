95c1975576910989f726d902bdd86c52f82f642d
snehasawant2019 <sawantsneha2019@gmail.com>
2023-02-06 15:11:34 +0530

    new

---
+++ src/com/test/Strings.java
@@ -0,0 +1,14 @@
+package com.test;
+
+public class Strings {
+
+		public static String getStudentName(String name) {
+		return name;
+		}
+		public static void main(String[] args) {
+		String e= getStudentName ("sneha");
+		System.out.println("student name is"+' '+e);
+		
+		
+		}
+	}
